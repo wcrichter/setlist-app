ffa55124f9057da0073ca0653636828097c5e87f
Chris Richter <wcrichter@gmail.com>
2017-04-18 09:22:23 -0400

    updated splash page withRoboto font and button

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Splash = (props) => {
   return(
-    <div id="splash" className="vh-100 dt w-100 tc bg-dark-gray white cover">
-      <div className="white tc center">
-        Intelligig
+    <div id="splash" className="dt w-100 tc">
+      <div className="dtc v-mid">
+        <div id="splash-logo" className="mb5">
+          <span className="logo-text-light">Intelli</span>
+          <span className="logo-text-heavy">gig</span>
+        </div>
+        <div><button>Login/Sign Up</button></div>
       </div>
     </div>
   )
